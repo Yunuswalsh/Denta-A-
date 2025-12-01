6d60b02ab7770dd666f117deaf40daf423c7846a
Yunuswalsh <159577321+Yunuswalsh@users.noreply.github.com>
2025-12-01 11:32:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Bu kısım "process is not defined" hatasını çözer (Beyaz Ekranın Sebebi)
+  define: {
+    'process.env': {} 
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 });
